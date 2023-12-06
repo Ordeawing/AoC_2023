d7ea8b145a4f536537cc90575a00e1ca65f58b9e
Ordeawing <romain88130@gmail.com>
2023-12-06 10:19:10 +0100

    WiP day 2 - Part 2

--- day_2/cube_conundrum.cpp
+++ day_2/cube_conundrum.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <regex>
 using namespace std;
 
 void add(string **array, string value, size_t *arr_size) {
@@ -42,6 +43,34 @@ void removeAt(string **array, int to_delete, size_t *arr_size) {
     *array = temp;
 }
 
+void checkValue(string set, int &game_id) {
+    regex red("([0-9]+) red");
+    regex green("([0-9]+) green");
+    regex blue("([0-9]+) blue");
+    smatch match;
+    int red_max = 12;
+    int green_max = 13;
+    int blue_max = 14;
+
+    if (regex_search(set, match, red)) {
+        int red_count = stoi(match[1].str());
+        if (red_count > red_max) {
+            game_id = 0;
+        }
+    }
+    if (regex_search(set, match, green)) {
+        int green_count = stoi(match[1].str());
+        if (green_count > green_max) {
+            game_id = 0;
+        }
+    }
+    if (regex_search(set, match, blue)) {
+        int blue_count = stoi(match[1].str());
+        if (blue_count > blue_max) {
+            game_id = 0;
+        }
+    }
+}
 
 int main () {
     fstream file;
@@ -50,6 +79,7 @@ int main () {
     size_t s_games = 0;
     string *games;
     games = new string[s_games];
+    int result = 0;
 
     if (file.is_open()) {
         string line;
@@ -61,9 +91,18 @@ int main () {
         }
 
         for (int i = 0; i < s_games; i++) {
-            cout << games[i] << '\n';
+            size_t pos;
+            string set;
+            int game_id = i + 1;
+            while ((pos = games[i].find(";")) != string::npos) {
+                set = games[i].substr(0, pos);
+                checkValue(set, game_id);
+                games[i].erase(0, pos + 1);
+            }
+            checkValue(games[i], game_id);
+            result += game_id;
         }
-
         file.close();
+        cout << result << endl;
     }
 }
